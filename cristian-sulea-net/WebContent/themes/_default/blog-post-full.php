96345a7f7dc965a20d476d51d6c3bb063eb3a57f
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-01-06 18:20:25 +0200

    no css in default theme

--- cristian-sulea-net/WebContent/themes/_default/blog-post-full.php
+++ cristian-sulea-net/WebContent/themes/_default/blog-post-full.php
@@ -7,7 +7,7 @@
 		<a href="<?php printPostAuthorWebsite(); ?>" title="<?php printPostAuthor(); ?>"><?php printPostAuthor(); ?></a>
 	</header>
 
-	<?php printPostContent('image'); ?>
+	<?php printPostContent(); ?>
 
 	<?php if (hasPostResources()) { ?>
 		<?php echo '<hr />'; ?>
