bb14e7c97f531c9eb43cabcafe0e5c5af9804b62
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-01-06 17:33:09 +0200

    settings in a separate file

--- cristian-sulea-net/WebContent/_include.php
+++ cristian-sulea-net/WebContent/_include.php
@@ -1,20 +1,6 @@
 <?php
 
-//
-// settings
-
-//define("THEME", "themes/html5up-future-imperfect");
-define("THEME", "themes/_default");
-
-define("TITLE", "Knowlegde Base");
-define("DESCRIPTION", "");
-define("KEYWORDS", "");
-
-define("BLOG_AUTHOR", "Cristian Sulea");
-define("BLOG_AUTHOR_EMAIL", "cristian.sulea.79@gmail.com");
-define("BLOG_AUTHOR_WEBSITE", "http://cristian.sulea.net");
-
-define("POST_DATE_FORMAT", "F j, Y");
+include("settings.php");
 
 if (file_exists(THEME . "/_include.php")) {
 	include(THEME . "/_include.php");
---
+++ cristian-sulea-net/WebContent/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+//define("THEME", "themes/html5up-future-imperfect");
+define("THEME", "themes/_default");
+
+define("TITLE", "Knowlegde Base");
+define("DESCRIPTION", "");
+define("KEYWORDS", "");
+
+define("BLOG_AUTHOR", "Cristian Sulea");
+define("BLOG_AUTHOR_EMAIL", "cristian.sulea.79@gmail.com");
+define("BLOG_AUTHOR_WEBSITE", "http://cristian.sulea.net");
+
+define("POST_DATE_FORMAT", "F j, Y");
+
+?>
