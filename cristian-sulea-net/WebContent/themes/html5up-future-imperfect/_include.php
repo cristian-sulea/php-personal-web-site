433b3571fb06a22a2e2cf22c8eb5963cbdd26d0b
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-01-12 12:43:46 +0200

    cleanup "content.html" -> add "image" class to <img> <a> parent is in theme implementation

--- cristian-sulea-net/WebContent/themes/html5up-future-imperfect/_include.php
+++ cristian-sulea-net/WebContent/themes/html5up-future-imperfect/_include.php
@@ -4,12 +4,32 @@
 // functions
 
 function themeUpdatePostContent($postContent) {
-
+	
+	//
+	// replace "<pre>" with "<pre><code>"
+	
 	$postContent = str_replace(
 							array('<pre>', '</pre>'),
 							array('<pre><code>', '</code></pre>'),
 							$postContent);
-
+	
+	//
+	// add "image" class to <img> <a> parent
+	
+	$dom = new DOMDocument();
+	$dom->loadHTML($postContent);
+	
+	foreach ( $dom->getElementsByTagName ( "img" ) as $img ) {
+		if ($img->parentNode->nodeName == "a") {
+			$img->parentNode->setAttribute ( "class", "image" );
+		}
+	}
+	
+	$postContent = preg_replace('/^<!DOCTYPE.+?>/', '', str_replace( array('<html>', '</html>', '<body>', '</body>'), array('', '', '', ''), $dom->saveHTML()));
+	
+	//
+	// return updated $postContent
+	
 	return $postContent;
 }
 
