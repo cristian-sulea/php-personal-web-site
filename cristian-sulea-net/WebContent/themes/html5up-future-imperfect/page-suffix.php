5c330821a6c34ad3e5203d38b87fdcd4ac180c7b
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-01-12 10:20:59 +0200

    fix for footer

--- cristian-sulea-net/WebContent/themes/html5up-future-imperfect/page-suffix.php
+++ cristian-sulea-net/WebContent/themes/html5up-future-imperfect/page-suffix.php
@@ -2,9 +2,13 @@
 
 		<footer id="sidebar">
 			<div id="footer">
-				<ul class="icons">
+				<?php if (isBlog()) { ?>
+					<!-- nothing to do (yet) -->
+				<?php } else { ?>
+					<ul class="icons">
 					<?php printProfiles(); ?>
 				</ul>
+				<?php } ?>
 				<p class="copyright">
 					&copy; <a href="http://cristian.sulea.net">Cristian Sulea</a>. Design: <a href="http://html5up.net">HTML5 UP</a>.
 				</p>
