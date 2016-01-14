2eb8998e7e71d30e40e6fe238d666c038ea64990
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-01-14 17:10:37 +0200

    refactoring: formatting

--- cristian-sulea-net/WebContent/blog.php
+++ cristian-sulea-net/WebContent/blog.php
@@ -12,30 +12,30 @@ if (count($params) > 0) {
 }
 
 if (isset($postId) && existsContentFile("blog/" . $postId)) {
-
+	
 	$isBlogPost = true;
-
+	
 	$postConfig  = json_decode(readContentFile("blog/" . $postId . "/config.json"), TRUE);
 	$blogPostContent = readContentFile("blog/" . $postId . "/content.html");
-
+	
 	includeThemeFile("page-prefix.php");
 	includeThemeFile("blog-post-full.php");
 	includeThemeFile("page-suffix.php");
 }
 
 else {
-
+	
 	$isBlogPost = false;
-
+	
 	includeThemeFile("page-prefix.php");
-
+	
 	foreach (array_diff(scandir("content/blog/", 1), array(".", "..")) as $file) {
-
+		
 		$postId = basename($file);
-
+		
 		$postConfig  = json_decode(readContentFile("blog/" . $postId . "/config.json"), TRUE);
 		$blogPostContent = readContentFile("blog/" . $postId . "/excerpt.html");
-
+		
 		includeThemeFile("blog-post-excerpt.php");
 	}
 	
