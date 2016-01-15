0848e7952f367e60f50ccfb6bd51d35dc0f73908
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-01-15 16:00:08 +0200

    search.php - development version

---
+++ cristian-sulea-net/WebContent/search.php
@@ -0,0 +1,16 @@
+
+<script>
+  (function() {
+    var cx = '008197242257401797086:73803bll01y';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:searchresults-only></gcse:searchresults-only>
+
+<iframe src="http://www.w3schools.com"></iframe>
