5a44d46c8561f4a3075112f1bf4e3702e555dd3a
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-01-21 11:14:57 +0200

    main.min.css

--- cristian-sulea-net/WebContent/themes/html5up-future-imperfect/page-prefix.php
+++ cristian-sulea-net/WebContent/themes/html5up-future-imperfect/page-prefix.php
@@ -21,7 +21,7 @@
 <link rel="shortlink" href="<?php printHtmlHeadLinkShortlink(); ?>">
 
 <!--[if lte IE 8]><script src="<?php printThemeFile('js/ie/html5shiv.js'); ?>"></script><![endif]-->
-<link rel="stylesheet" href="<?php printThemeFile('css/main.css'); ?>" />
+<link rel="stylesheet" href="<?php printThemeFile('css/main.min.css'); ?>" />
 <!--[if lte IE 9]><link rel="stylesheet" href="<?php printThemeFile('css/ie9.css'); ?>" /><![endif]-->
 <!--[if lte IE 8]><link rel="stylesheet" href="<?php printThemeFile('css/ie8.css'); ?>" /><![endif]-->
 
