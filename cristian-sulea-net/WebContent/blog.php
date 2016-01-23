16934715714fc35e8fa4028235c500857a15024b
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-01-23 18:38:22 +0200

    blog update

--- cristian-sulea-net/WebContent/blog.php
+++ cristian-sulea-net/WebContent/blog.php
@@ -1,50 +1,45 @@
 <?php
-header('Content-Type: text/html; charset=UTF-8');
+include '__include.php';
 
-include "__include.php";
+setContentTypeHTML();
+setIsBlog();
 
-$isBlog = true;
-
-include "_include-blog.php";
+include '_include-blog.php';
 
 if (isset($_GET[$BLOG_POST_ID_PARAM])) {
-	$postId = $_GET[$BLOG_POST_ID_PARAM];
-}
-
-if (isset($postId)) {
 	
-	if (!existsBlogPost($postId) || empty($postId)) {
+	setBlogPostId($_GET[$BLOG_POST_ID_PARAM]);
+	
+	if (!existsBlogPost()) {
 		header('Location: blog.php');
 		exit();
 	}
 	
-	$isBlogPost = true;
+	setIsBlogPost();
 	
-	$postConfig = readBlogPostConfig($postId);
-	$blogPostContent = readBlogPostContent($postId);
+	readBlogPostConfig();
+	readBlogPostContent();
 	
-	includeThemeFile("page-prefix.php");
-	includeThemeFile("blog-post-full.php");
-	includeThemeFile("page-suffix.php");
+	includeThemeFile('page-prefix.php');
+	includeThemeFile('blog-post-full.php');
+	includeThemeFile('page-suffix.php');
 }
 
 else {
 	
-	$isBlogPost = false;
-	
-	includeThemeFile("page-prefix.php");
+	includeThemeFile('page-prefix.php');
 	
 	foreach (getBlogPostFolders() as $blogPostFolder) {
 		
-		$postId = basename($blogPostFolder);
+		setBlogPostId(basename($blogPostFolder));
 		
-		$postConfig = readBlogPostConfig($postId);
-		$blogPostContent = readBlogPostExcerpt($postId);
+		readBlogPostConfig();
+		readBlogPostContent(true);
 		
-		includeThemeFile("blog-post-excerpt.php");
+		includeThemeFile('blog-post-excerpt.php');
 	}
 	
-	includeThemeFile("page-suffix.php");
+	includeThemeFile('page-suffix.php');
 }
 
 ?>
