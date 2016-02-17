180ded4133dd7683feb15f1cb10b2f6a4034df62
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-02-17 15:38:20 +0200

    use description instead of excerpt file

---
+++ php-personal-web-page-with-blog/WebContent/themes/html5up-future-imperfect/blog-post-description.php
@@ -0,0 +1,27 @@
+
+<article class="post">
+
+	<header>
+
+		<div class="title">
+			<h2><a href="<?php printBlogPostLink(); ?>"><?php printBlogPostTitle(); ?></a></h2>
+		</div>
+
+		<div class="meta">
+			<time datetime="<?php printBlogPostDateForHtmlTimeTag(); ?>" class="published"><?php printBlogPostDate(); ?></time>
+			<a href="<?php printBlogPostAuthorWebsite(); ?>" title="<?php printBlogPostAuthor(); ?>" class="author">
+				<span class="name"><?php printBlogPostAuthor(); ?></span><img src="<?php printBlogPostAuthorEmail(); ?>" alt="" />
+			</a>
+		</div>
+
+	</header>
+
+	<p><?php printBlogPostDescription(); ?></p>
+
+	<footer>
+		<ul class="actions">
+			<li><a href="<?php printBlogPostLink(); ?>" class="button big">Continue Reading</a></li>
+		</ul>
+	</footer>
+
+</article>
