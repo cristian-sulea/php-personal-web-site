c7b50749ed83db2664832968a9d61cf80e4c96d4
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-02-25 14:42:17 +0200

    some fixes in function updateBlogPostContent() to remove the automatically "p" tag added by $dom

--- php-personal-web-page-with-blog/WebContent/themes/html5up-future-imperfect/_include-theme.php
+++ php-personal-web-page-with-blog/WebContent/themes/html5up-future-imperfect/_include-theme.php
@@ -48,7 +48,7 @@ function updateBlogPostContent($blogPostContentBuffer) {
 	// add "image" class to <img> parent (<a>)
 	
 	$dom = new DOMDocument();
-	$dom->loadHTML($blogPostContentBuffer);
+	$dom->loadHTML('<div id="xxx-unique-div-container-yyy">' . $blogPostContentBuffer . '</div>');
 	
 	foreach ( $dom->getElementsByTagName ( "img" ) as $img ) {
 		if ($img->parentNode->nodeName == "a") {
@@ -57,6 +57,11 @@ function updateBlogPostContent($blogPostContentBuffer) {
 	}
 	
 	$blogPostContentBuffer = preg_replace('/^<!DOCTYPE.+?>/', '', str_replace( array('<html>', '</html>', '<body>', '</body>'), array('', '', '', ''), $dom->saveHTML()));
+	//$blogPostContentBuffer = $dom->saveHTML($dom->getElementById('xxx-unique-div-container-yyy'));
+	
+	$blogPostContentBuffer = trim($blogPostContentBuffer);
+	$blogPostContentBuffer = substr($blogPostContentBuffer, 39, strlen($blogPostContentBuffer) - 39 - 6);
+	$blogPostContentBuffer = trim($blogPostContentBuffer);
 	
 	//
 	// return updated content
