adb207837f9356ea81c5083722bbd2ad73230dd4
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-03-18 16:42:43 +0200

    working on default version

--- php-personal-web-site/sync-xampp.bat
+++ php-personal-web-site/sync-xampp.bat
@@ -1,2 +1,3 @@
 robocopy WebContent c:\Apps\xampp\htdocs /MIR
+rem robocopy ..\..\cristian-sulea-net\cristian-sulea-net\WebContent\config c:\Apps\xampp\htdocs\config /MIR
 rem robocopy ..\..\cristian-sulea-net\cristian-sulea-net\WebContent\content c:\Apps\xampp\htdocs\content /MIR
