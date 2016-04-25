7a96137831867e84fbef4a751231e82569dc4a77
Cristian Sulea <cristian.sulea.79@gmail.com>
2016-04-25 12:56:06 +0300

    open search icon

--- php-personal-web-site/WebContent/opensearch.php
+++ php-personal-web-site/WebContent/opensearch.php
@@ -6,8 +6,13 @@ setContentTypeXML();
 echo '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL;
 echo '<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">' . PHP_EOL;
 
-echo '	<ShortName>'   . getBlogTitle()       . '</ShortName>' . PHP_EOL;
+echo '	<ShortName>'   . getBlogTitle()       . '</ShortName>'   . PHP_EOL;
 echo '	<Description>' . getBlogDescription() . '</Description>' . PHP_EOL;
+
+echo '	<Image width="16" height="16" type="image/png">' . getAbsoluteLink(getBlogIcon()) . '</Image>' . PHP_EOL;
+
+echo '	<InputEncoding>UTF-8</InputEncoding>'   . PHP_EOL;
+echo '	<OutputEncoding>UTF-8</OutputEncoding>' . PHP_EOL;
 
 echo '	<Url type="text/html" template="' . getAbsoluteLink(getBlogLink()) . '?' . getBlogSearchParam() . '={searchTerms}"/>' . PHP_EOL;
 
